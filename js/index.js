9e6390aa3b32abc808f0f85389c51e8d2c7b6ba6
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2015-12-20 15:57:13 +0000

    Set up absolute positions

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-var letters = document.querySelector('#container span');
+var letters = document.querySelectorAll('#container span');
 
 var p = letters[0];
 
-/*
 new mojs.Tween({
   repeat: 999,
   delay: 500,
-});
-*/
+  onUpdate: function(progress) {
+    var bounceProgress = mojs.easing.bounce.out(progress);
+    p.style.transform = 'translateY(' + 200*bounceProgress + 'px)';
+  }
+})//.run();
