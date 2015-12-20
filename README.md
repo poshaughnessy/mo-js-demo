ff31fbbb67d5a6f327041d4505e7bd4fb9a2a650
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2015-12-20 18:51:55 +0000

    Added audio

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mo-js-demo
+
+Trying out [mo.js](http://mojs.io/).
+
+Sound based on ["Ping Pong Ball Bouncing" by Maynardkenmuir](https://freesound.org/people/Maynardkenmuir/sounds/328108/),
+[CC Attribution Noncommercial](http://creativecommons.org/licenses/by-nc/3.0/).
--- js/index.js
+++ js/index.js
@@ -3,12 +3,20 @@ var halfWindowHeight = windowHeight / 2;
 var letters = document.querySelectorAll('#container span');
 var bounceTweens = new mojs.Timeline();
 
+var kickSound = new Howl({urls: ['sounds/ping-pong-bounce.mp3', 'sounds/ping-pong-bounce.wav'], volume: 0.2})
+
 function createBounceTween(i) {
 
   var letter = letters[i];
 
   return new mojs.Tween({
-    delay: i * 500,
+    delay: i * 400,
+    duration: 500,
+    onStart: function() {
+      setTimeout(function() {
+        kickSound.play();
+      }, 200);
+    },
     onUpdate: function(progress) {
       var bounceProgress = mojs.easing.bounce.out(progress);
       letter.style.transform = 'translateY(' + (halfWindowHeight * bounceProgress - 100) + 'px)';
