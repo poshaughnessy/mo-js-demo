9ea8c1ec200792af6653f7a19e56e2905bf09726
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2015-12-20 20:08:54 +0000

    Change timing function

--- js/index.js
+++ js/index.js
@@ -8,9 +8,10 @@ var kickSound = new Howl({urls: ['sounds/ping-pong-bounce.mp3', 'sounds/ping-pon
 function createBounceTween(i) {
 
   var letter = letters[i];
+  var letterProgress = i / letters.length;
 
   return new mojs.Tween({
-    delay: 500 + i * 300,
+    delay: 500 + 1000 * (letterProgress * (2-letterProgress)),
     duration: 500,
     onStart: function() {
       setTimeout(function() {
